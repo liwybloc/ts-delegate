ef52cff8674d88fec638ad1d2fa3a7226f16f05e
liwy <208813904+liwybloc@users.noreply.github.com>
2025-09-03 17:14:07 -0600

    create!!!

--- README.md
+++ README.md
@@ -1,2 +1,138 @@
 # ts-delegate
- Helper annotations, types, and methods for type-safe and jsdoc'd easy delegating in typescript
+ Helper annotations, types, and methods for type-safe and JSDoc-preserving delegation in TypeScript
+
+## Installation
+Run command: `npm i ts-delegate`<br>
+<br>
+This library also requires these options in tsconfig.json<br>
+```json
+{
+    "compilerOptions": {
+        ...
+        "experimentalDecorators": true,
+        "emitDecoratorMetadata": true
+    },
+    ...
+}
+```
+### Without annotations:
+If you can't use annotations, you can replace any documented case of @DelegateMethod() with this:
+```ts
+class MyClass {
+    method() {
+        ...
+    }
+    static method2() {
+        ...
+    }
+    constructor() {
+        // this.method() will now be delegated when delegate() is used on this class
+        (this.method as any).isDelegated = true;
+    }
+}
+// for static methods
+(typeof MyClass.method2).isDelegated = true;
+(typeof MyClass.method2).omitFirst = true;
+```
+
+## What It Does
+`ts-delegate` helps you:
+
+- Delegate methods from other classes without duplicating JSDoc or type definitions.
+- Preserve type safety for delegated methods.
+- Delegate static methods into instances and optionally omit the first argument to pass `this`.
+
+## Usage
+
+### Marking for delegation
+Methods can be marked for delegation with @DelegateMethod()
+
+#### Instance
+Does not need any arguments; supplying them won't do anything
+```ts
+class MyClass {
+    // when doing Delegate<[MyClass]> this will be delegated
+    @DelegateMethod()
+    method() {
+        ...
+    }
+    // but this one will not
+    thisWontBeDelegated() {
+        ...
+    }
+}
+```
+#### Static
+For static functions, you can do the same, and optionally add `true` for omitting the first argument
+```ts
+class Static {
+    @DelegateMethod()
+    static method1() {
+        ...
+    }
+
+    // when this method is called in delegated method MyClass#method2, it will call with the MyClass instance as the first argument
+    @DelegateMethod(true)
+    static method2(other: MyClass) {
+        ...
+    }
+}
+```
+
+### Implementing delegated functions
+You can delegate with functions and a type
+#### Instance
+For delegating instances, you do Delegate<[array of classes]> and delegate(this, [array of instances]);
+```ts
+class OtherClass {
+    @DelegateMethod()
+    myMethod() {
+        ...
+    }
+}
+class MyClass implements Delegate<[OtherClass]> {
+    constructor() {
+        delegate(this, [new OtherClass()]);
+
+        this.myMethod();
+    }
+
+    // This will confirm to typescript that it exists and will clone the structure of the other method, this still retains JSDoc and all functionality
+    declare myMethod: OtherClass['myMethod'];
+}
+```
+#### Static
+For delegating static classes, you do DelegateStatic<[array of classes], (this)> with (this) as the class implementing it, and delegateStatic(this, [array of classes]);
+```ts
+class StaticClass {
+    @DelegateMethod()
+    static add(a: number, b: number): number {
+        return a + b;
+    }
+
+    // This will omit the first argument and it will be called with `this` in MyClass
+    // This is useful if you want to have helper classes in different files that can interact with your class without requiring an instance of the helper class to be made
+    @DelegateMethod(true)
+    static change(myClass: MyClass, to: number): void {
+        myClass.value = to;
+    }
+}
+class MyClass implements DelegateStatic<[StaticClass], MyClass> {
+    value: number = 0;
+
+    constructor() {
+        delegateStatic(this, [StaticClass]);
+
+        console.log(this.add(5, 10)) // 15
+
+        // first argument is omitted and replaced with `this`
+        this.change(100);
+        console.log(this.value); // 100
+    }
+
+    // declare for static requires typeof
+    declare add: typeof StaticClass.add;
+    // OmitFirst<> to omit the first argument for this
+    declare change: OmitFirst<typeof StaticClass.change>;
+}
+```
---
+++ src/delegate.ts
@@ -0,0 +1,189 @@
+/**
+ * Adds Intellisense and JSDoc when implemented. This delegates instanced methods from a class.
+ * 
+ * For the generic, an array of classes should be supplied and the types will be added from them.
+ * 
+ * @example
+ * ```ts
+ * class ClassOne {
+ *   value: number;
+ * 
+ *   constructor() {
+ *     this.value = 0;
+ *   }
+ * 
+ *   @DelegateMethod()
+ *   doSomething() {
+ *     this.value++;
+ *   }
+ * }
+ * class ClassTwo implements Delegate<[ClassOne]> {
+ *   value: number = 0;
+ *   constructor() {
+ *     delegate(this, [new ClassOne()]);
+ * 
+ *     this.doSomething();
+ *   }
+ * 
+ *   // Confirms to typescript that it exists
+ *   declare doSomething: ClassOne['doSomething'];
+ * }
+ * ```
+ */
+export type Delegate<T extends any[]> = T extends [infer First, ...infer Rest]
+  ? Partial<{ [K in keyof First]: First[K]; }> & Delegate<Rest>
+  : {};
+
+type IsExactly<T, U> = (<G>() => G extends T ? 1 : 2) extends (<G>() => G extends U ? 1 : 2) ? true : false;
+
+/**
+ * Omits the first argument from a function
+ */
+export type OmitFirst<F> =
+  F extends (arg: any, ...rest: infer R) => infer Ret
+    ? (...args: R) => Ret
+    : F;
+
+type OmitFirstIfSelf<F, Self> =
+  F extends (arg: infer First, ...rest: any[]) => any
+    ? IsExactly<First, Self> extends true
+      ? OmitFirst<F>
+      : F
+    : F;
+
+type DelegateFromCtor<C, Self> = Partial<{
+  [K in keyof C as C[K] extends (...args: any[]) => any ? K : never]: OmitFirstIfSelf<C[K], Self>
+}>;
+
+/**
+ * Adds Intellisense and JSDoc when implemented. This delegates static methods from a class.
+ * 
+ * For the first generic, an array of classes should be supplied and the types will be added from them.
+ * 
+ * And for the second generic, it is the current class itself. This will then omit the first argument of any functions that take self first.
+ * 
+ * @example
+ * ```ts
+ * class ClassOne {
+ *   @DelegateMethod(true)
+ *   static doSomething(two: ClassTwo, amount: number) {
+ *     two.value += amount;
+ *   }
+ * }
+ * class ClassTwo implements DelegateStatic<[ClassOne], ClassTwo> {
+ *   value: number = 0;
+ *   constructor() {
+ *     delegateStatic(this, [ClassOne]);
+ * 
+ *     this.doSomething(10); // first argument is omitted and will call with 'this'
+ *   }
+ * 
+ *   // This confirms to typescript that it exists
+ *   // In this case since omitFirst is true, the type here needs to omit the first one to be accurate for the instanced delegated function
+ *   declare doSomething: OmitFirst<ClassOne['doSomething']>;
+ * }
+ * ```
+ */
+export type DelegateStatic<T extends readonly any[], Self> =
+  T extends [infer First, ...infer Rest]
+    ? First extends abstract new (...args: any) => any
+      ? DelegateFromCtor<First, Self> & DelegateStatic<Rest, Self>
+      : DelegateStatic<Rest, Self>
+    : {};
+
+/**
+ * Adds instanced delegated methods to a class; called during runtime
+ * @param self Instance to add delegated methods to
+ * @param delegateCandidates Array of instances for delegation
+ * 
+ * @example
+ * ```ts
+ * class ClassOne {
+ *   n: number;
+ *   constructor() {
+ *     this.n = Math.random();
+ *   }
+ *   @DelegateMethod()
+ *   myMethod(): void {
+ *     console.log("Hello world!", this.n);
+ *   }
+ * }
+ * class ClassTwo implements Delegate<[ClassOne]> {
+ *   constructor() {
+ *     delegateStatic(this, [new ClassOne()]);
+ *   }
+ *   
+ *   // This will copy over type safety and JSDoc
+ *   declare myMethod: ClassOne['myMethod'];
+ * }
+ * ```
+ */
+export function delegate(self: any, delegateCandidates: any[]) {
+    for(const candidate of delegateCandidates) {
+        for (const key in Object.getPrototypeOf(candidate)) {
+            if(!candidate[key].isDelegated) continue;
+            self[key] = candidate[key].bind(candidate);
+        }
+    }
+}
+
+/**
+ * Adds static delegate methods to a class; called during runtime
+ * @param self Instance to add delegated methods to
+ * @param delegateCandidates Array of classes for delegation, this is the name of the class and not with .prototype or typeof
+ * 
+ * @example
+ * ```ts
+ * class ClassOne {
+ *   @DelegateMethod()
+ *   static myMethod(): void {
+ *     console.log("Hello world!");
+ *   }
+ * }
+ * class ClassTwo implements DelegateStatic<[ClassOne], ClassTwo> {
+ *   constructor() {
+ *     delegateStatic(this, [ClassOne]);
+ *   }
+ *   
+ *   // This will copy over type safety and JSDoc
+ *   declare myMethod: ClassOne['myMethod'];
+ * }
+ * ```
+ */
+export function delegateStatic(self: any, delegateCandidates: any[]) {
+    for(const candidate of delegateCandidates) {
+        for (const key in candidate) {
+            const val = candidate[key];
+            if (typeof val != "function" || !val.isDelegated) continue;
+
+            self[key] = (...args: any[]) => val.apply(candidate, val.omitFirst ? [self, ...args] : args);
+        }
+    }
+}
+
+/**
+ * Annotate methods with this to set them to delegate into other methods
+ * @param omitFirst If true, the first argument of this method will be omitted when delegating
+ *                  to the target method. It will be replaced with the instance of the class that delegated it.
+ *                  This only applies for static methods.
+ * 
+ * @example
+ * ```ts
+ * class Example {
+ *   @DelegateMethod() // an instance delegating this method will be able to use this.exampleMethod(a, b);
+ *   exampleMethod(a: number, b: number): number {
+ *     return a + b;
+ *   }
+ *   @DelegateMethod(true) // an instance delegating this method will be able to use this.editSelf();
+ *   static editSelf(self: any): void {
+ *     self.something = Math.random();
+ *   }
+ * }
+ * ```
+ */
+export function DelegateMethod(omitFirst: boolean = false): MethodDecorator {
+    return (_: Object, __: string | symbol, descriptor: PropertyDescriptor) => {
+        (descriptor.value as any).isDelegated = true;
+        (descriptor.value as any).omitFirst = omitFirst;
+    };
+}
